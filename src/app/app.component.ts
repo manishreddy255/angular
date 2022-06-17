2d5e360709dbf112237021023bdb646d34c0cdc1
manishreddy255 <mainsreddy255@gmail.com>
2022-06-17 10:39:27 +0530

    liberation of html and css :p

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `<p>hai we are in browser</p>`
+  // template: `<p>hai we are in browser</p>`,
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  // styles:[]
 })
 export class AppComponent{
 
